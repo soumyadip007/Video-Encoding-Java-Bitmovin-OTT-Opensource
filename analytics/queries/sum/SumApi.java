8718b5663a9e59d8e8454b1911566e9237b694da
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2019-05-14 01:45:20 +0530

    Create SumApi.java

---
+++ analytics/queries/sum/SumApi.java
@@ -0,0 +1,62 @@
+package com.bitmovin.api.sdk.analytics.queries.sum;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import feign.Param;
+import feign.QueryMap;
+import feign.RequestLine;
+import feign.Body;
+import feign.Headers;
+
+import com.bitmovin.api.sdk.model.*;
+import com.bitmovin.api.sdk.common.BitmovinException;
+import static com.bitmovin.api.sdk.common.BitmovinExceptionFactory.buildBitmovinException;
+import com.bitmovin.api.sdk.common.BitmovinDateExpander;
+import com.bitmovin.api.sdk.common.BitmovinApiBuilder;
+import com.bitmovin.api.sdk.common.BitmovinApiClientFactory;
+
+public class SumApi {
+
+    private final SumApiClient apiClient;
+
+    public SumApi(BitmovinApiClientFactory clientFactory) {
+        if (clientFactory == null)
+        {
+            throw new IllegalArgumentException("Parameter 'clientFactory' may not be null.");
+        }
+
+        this.apiClient = clientFactory.createApiClient(SumApiClient.class);
+
+    }
+
+    /**
+     * Fluent builder for creating an instance of SumApi
+     */
+    public static BitmovinApiBuilder<SumApi> builder() {
+        return new BitmovinApiBuilder<>(SumApi.class);
+    }
+    
+    /**
+     * Sum
+     * 
+     * @param analyticsSumQueryRequest Analytics Query Object (required)
+     * @return AnalyticsResponse
+     * @throws BitmovinException if fails to make API call
+     */
+    public AnalyticsResponse create(AnalyticsSumQueryRequest analyticsSumQueryRequest) throws BitmovinException {
+        try {
+            return this.apiClient.create(analyticsSumQueryRequest).getData().getResult();
+        } catch (Exception ex) {
+            throw buildBitmovinException(ex);
+        }
+    }
+    
+    interface SumApiClient {
+    
+        @RequestLine("POST /analytics/queries/sum")
+        ResponseEnvelope<AnalyticsResponse> create(AnalyticsSumQueryRequest analyticsSumQueryRequest) throws BitmovinException;
+    }
+}

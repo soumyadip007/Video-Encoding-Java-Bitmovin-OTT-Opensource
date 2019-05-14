beb7ce713e73b79a12545827964fd4f3e88ec0af
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2019-05-15 01:49:42 +0530

    Create CaptionsApi.java

---
+++ encoding/encodings/captions/CaptionsApi.java
@@ -0,0 +1,27 @@
+package com.bitmovin.api.sdk.encoding.encodings.captions;
+
+import com.bitmovin.api.sdk.common.BitmovinApiBuilder;
+import com.bitmovin.api.sdk.common.BitmovinApiClientFactory;
+import com.bitmovin.api.sdk.encoding.encodings.captions.scc.SccApi;
+
+public class CaptionsApi {
+    public final SccApi scc;
+
+    public CaptionsApi(BitmovinApiClientFactory clientFactory) {
+        if (clientFactory == null)
+        {
+            throw new IllegalArgumentException("Parameter 'clientFactory' may not be null.");
+        }
+
+        this.scc = new SccApi(clientFactory);
+    }
+
+    /**
+     * Fluent builder for creating an instance of CaptionsApi
+     */
+    public static BitmovinApiBuilder<CaptionsApi> builder() {
+        return new BitmovinApiBuilder<>(CaptionsApi.class);
+    }
+    
+    
+}

c7221a06bbb1a1231464673b714230197350ac52
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2019-05-22 04:07:55 +0530

    Create TypeApi.java

---
+++ encoding/filters/type/TypeApi.java
@@ -0,0 +1,62 @@
+package com.bitmovin.api.sdk.encoding.filters.type;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import feign.Param;
+import feign.QueryMap;
+import feign.RequestLine;
+import feign.Body;
+import feign.Headers;
+
+import com.bitmovin.api.sdk.model.*;
+import com.bitmovin.api.sdk.common.BitmovinException;
+import static com.bitmovin.api.sdk.common.BitmovinExceptionFactory.buildBitmovinException;
+import com.bitmovin.api.sdk.common.BitmovinDateExpander;
+import com.bitmovin.api.sdk.common.BitmovinApiBuilder;
+import com.bitmovin.api.sdk.common.BitmovinApiClientFactory;
+
+public class TypeApi {
+
+    private final TypeApiClient apiClient;
+
+    public TypeApi(BitmovinApiClientFactory clientFactory) {
+        if (clientFactory == null)
+        {
+            throw new IllegalArgumentException("Parameter 'clientFactory' may not be null.");
+        }
+
+        this.apiClient = clientFactory.createApiClient(TypeApiClient.class);
+
+    }
+
+    /**
+     * Fluent builder for creating an instance of TypeApi
+     */
+    public static BitmovinApiBuilder<TypeApi> builder() {
+        return new BitmovinApiBuilder<>(TypeApi.class);
+    }
+    
+    /**
+     * Get Filter Type
+     * 
+     * @param filterId Id of the filter (required)
+     * @return FilterType
+     * @throws BitmovinException if fails to make API call
+     */
+    public FilterType get(String filterId) throws BitmovinException {
+        try {
+            return this.apiClient.get(filterId).getData().getResult();
+        } catch (Exception ex) {
+            throw buildBitmovinException(ex);
+        }
+    }
+    
+    interface TypeApiClient {
+    
+        @RequestLine("GET /encoding/filters/{filter_id}/type")
+        ResponseEnvelope<FilterType> get(@Param(value = "filter_id") String filterId) throws BitmovinException;
+    }
+}

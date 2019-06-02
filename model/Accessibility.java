0184691e9a16834d1c6b2b46f30b5e9dc4526361
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2019-06-03 04:23:39 +0530

    Create Accessibility.java

---
+++ model/Accessibility.java
@@ -0,0 +1,100 @@
+package com.bitmovin.api.sdk.model;
+
+import java.util.Objects;
+import java.util.Arrays;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * Accessibility
+ */
+
+public class Accessibility {
+  @JsonProperty("value")
+  private String value;
+
+  @JsonProperty("schemeIdUri")
+  private String schemeIdUri;
+
+
+  /**
+   * Can be either list of languages or a complete map of services (or CC channels, in CEA-608 terminology) (required)
+   * @return value
+   */
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * Can be either list of languages or a complete map of services (or CC channels, in CEA-608 terminology) (required)
+   *
+   * @param value
+   *        Can be either list of languages or a complete map of services (or CC channels, in CEA-608 terminology) (required)
+   */
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+
+  /**
+   * The scheme id to use. Please refer to the DASH standard. (required)
+   * @return schemeIdUri
+   */
+  public String getSchemeIdUri() {
+    return schemeIdUri;
+  }
+
+  /**
+   * The scheme id to use. Please refer to the DASH standard. (required)
+   *
+   * @param schemeIdUri
+   *        The scheme id to use. Please refer to the DASH standard. (required)
+   */
+  public void setSchemeIdUri(String schemeIdUri) {
+    this.schemeIdUri = schemeIdUri;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Accessibility accessibility = (Accessibility) o;
+    return Objects.equals(this.value, accessibility.value) &&
+        Objects.equals(this.schemeIdUri, accessibility.schemeIdUri);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value, schemeIdUri);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Accessibility {\n");
+    
+    sb.append("    value: ").append(toIndentedString(value)).append("\n");
+    sb.append("    schemeIdUri: ").append(toIndentedString(schemeIdUri)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
